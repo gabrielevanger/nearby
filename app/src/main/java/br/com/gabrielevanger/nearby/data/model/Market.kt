392bcb3406358cdce5e505160708942ac43f9607
Gabriel Evangelista <gabriel.evanger@gmail.com>
2025-10-18 22:33:23 -0400

    feat(market): Add NearbyMarketCard componentAdds a new card component to display market information. This card will be used on the market listing screen.

---
+++ app/src/main/java/br/com/gabrielevanger/nearby/data/model/Market.kt
@@ -0,0 +1,15 @@
+package br.com.gabrielevanger.nearby.data.model
+
+data class Market(
+    val id: String,
+    val categoryId: String,
+    val name: String,
+    val description: String,
+    val cupons: Int,
+    val rules: List<Rule> = emptyList(),
+    val latitude: Double,
+    val longitude: Double,
+    val address: String,
+    val phone: String,
+    val cover: String,
+)
